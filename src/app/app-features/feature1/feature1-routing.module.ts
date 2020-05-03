766982deecf751cc5c8c3f7d88aac71bbfcc28bf
Saurabh Raja <saurabh@yumigo.in>
2020-05-03 18:04:21 +0530

    angular boilerplate updated

--- src/app/app-features/feature1/feature1-routing.module.ts
+++ src/app/app-features/feature1/feature1-routing.module.ts
@@ -1,3 +1,4 @@
+import { Component1Component } from './components/component1/component1.component';
 import { Feature1Component } from './feature1.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,7 +6,13 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    component: Feature1Component
+    component: Feature1Component,
+    children: [
+      {
+        path: 'component1',
+        component: Component1Component
+      }
+    ]
   }
 ];
 @NgModule({
--- src/app/app-features/feature1/feature1.module.ts
+++ src/app/app-features/feature1/feature1.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { Feature1Component } from './feature1.component';
 import { AppSharedModule } from 'src/app/app-shared/app-shared.module';
 import { Feature1RoutingModule } from './feature1-routing.module';
+import { Component1Component } from './components/component1/component1.component';
 
 @NgModule({
-  declarations: [Feature1Component],
+  declarations: [Feature1Component, Component1Component],
   imports: [
     CommonModule,
     AppSharedModule,
